69891d5a9960c7859e20c0425d3bb30f0f8d488f
zhendewokusi <768672249@qq.com>
2024-06-05 16:24:05 +0800

    暂存

--- 1.cc
+++ 1.cc
@@ -171,7 +171,7 @@ int main() {
     srand(static_cast<unsigned>(time(0)));
     // 明文
     string message = "26224012ZFY";
-    string message = "26224011LYT";
+//     string message = "26224011LYT";
 
     // 调整明文长度
     while (message.length() % 2 != 0) {
